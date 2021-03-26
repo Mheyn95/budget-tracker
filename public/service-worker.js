d2cc65b9c614648fbf69b7bec8b02cc5d92a79c3
mheyn95 <mheyn95@gmail.com>
2021-03-26 16:36:25 -0600

    fixed service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -9,11 +9,11 @@ const FILES_TO_CACHE = [
 ];
 
 // pull from cache first and fall back on network if cache does not exist
-// TODO: cache is registered and then created but will not load onto page when offline, seems like it doesn't exist
+// TODO: cache is registered and then created but will not load onto page when offline, seems like cache doesn't exist
 self.addEventListener("fetch", function (e) {
   e.respondWith(
-    caches.match(e.request).then(function (response) {
-      return response || fetch(e.request);
+    caches.match(e.request).then(function (request) {
+      return request || fetch(e.request);
     })
   );
 });
@@ -24,30 +24,6 @@ self.addEventListener("install", function (e) {
     caches.open(CACHE_NAME).then(function (cache) {
       console.log("installing cache : " + CACHE_NAME);
       return cache.addAll(FILES_TO_CACHE);
-    })
-  );
-});
-
-// Delete outdated caches
-self.addEventListener("activate", function (e) {
-  e.waitUntil(
-    caches.keys().then(function (keyList) {
-      // `keyList` contains all cache names under your username.github.io
-      // filter out ones that has this app prefix to create keeplist
-      let cacheKeeplist = keyList.filter(function (key) {
-        return key.indexOf(APP_PREFIX);
-      });
-      // add current cache name to keeplist
-      cacheKeeplist.push(CACHE_NAME);
-
-      return Promise.all(
-        keyList.map(function (key, i) {
-          if (cacheKeeplist.indexOf(key) === -1) {
-            console.log("deleting cache : " + keyList[i]);
-            return caches.delete(keyList[i]);
-          }
-        })
-      );
     })
   );
 });
